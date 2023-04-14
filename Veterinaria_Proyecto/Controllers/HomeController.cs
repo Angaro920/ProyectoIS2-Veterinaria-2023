1c65cef155b06166d3fe93bb334462258d01bd1f
ANDRES SANTIAGO CRUZ ARDILA <ascruz@ucundinamarca.edu.co>
2023-04-14 17:05:35 -0500

    Finalizar sesion

--- Veterinaria_Proyecto/Controllers/HomeController.cs
+++ Veterinaria_Proyecto/Controllers/HomeController.cs
@@ -31,6 +31,11 @@ namespace Veterinaria_Proyecto.Controllers
         {
             return View();
         }
+        public IActionResult finalizarSec()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
