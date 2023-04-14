34640f77c37ea9da8c177e2ab57bcc2179d6952a
ANDRES SANTIAGO CRUZ ARDILA <ascruz@ucundinamarca.edu.co>
2023-04-14 18:38:32 -0500

    Gestion de productos

--- Veterinaria_Proyecto/Controllers/HomeController.cs
+++ Veterinaria_Proyecto/Controllers/HomeController.cs
@@ -39,7 +39,10 @@ namespace Veterinaria_Proyecto.Controllers
         {
             return View();
         }
-
+        public IActionResult GestorProd()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
