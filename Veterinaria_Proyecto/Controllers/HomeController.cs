4969a82459ebc3017d3b2d7bd2a4ac5134a6629a
ANDRES SANTIAGO CRUZ ARDILA <ascruz@ucundinamarca.edu.co>
2023-04-14 16:27:24 -0500

    movimientos

--- Veterinaria_Proyecto/Controllers/HomeController.cs
+++ Veterinaria_Proyecto/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Veterinaria_Proyecto.Controllers
         {
             return View();
         }
+        public IActionResult IndexV()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
