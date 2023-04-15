139b81cc4dd5c2f413ec271fce80642684facce5
Angaro920 <95058616+Angaro920@users.noreply.github.com>
2023-04-14 20:01:18 -0500

    Add Dashboard

--- Veterinaria_Proyecto/Controllers/HomeController.cs
+++ Veterinaria_Proyecto/Controllers/HomeController.cs
@@ -43,6 +43,10 @@ namespace Veterinaria_Proyecto.Controllers
         {
             return View();
         }
+        public IActionResult dashboard()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
