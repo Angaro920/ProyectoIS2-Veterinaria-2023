dc335b60a49546866ddae0cc1e7f2462f04bb9a2
ANDRES SANTIAGO CRUZ ARDILA <ascruz@ucundinamarca.edu.co>
2023-05-17 17:05:40 -0500

    Login

--- Veterinaria_Proyecto/wwwroot/js/site.js
+++ Veterinaria_Proyecto/wwwroot/js/site.js
@@ -1,4 +1,45 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var token = "";
+var Rol = "";
 
-// Write your JavaScript code.
+function login() {
+
+    var queryURL = "https://localhost:44368/api/Authentication/requestToken";
+    var item = {
+        "username": document.getElementById("floatingInput").value,
+        "password": document.getElementById("floatingPassword").value
+    };
+    fetch(queryURL, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then((response) => response.json()).then((data) => {
+            console.log("Respuesta Login");
+            console.log(data);
+            console.log(data.token);
+            if (data.token != null) {
+                location.href = 'https://localhost:44326/dashboard/Index';
+            } else {
+                location.href = 'https://localhost:44326/home/login';
+            }
+            if (data) {           
+                token = data.token;
+                remoteResponse.innerText = data.mensaje;
+               
+               
+            }
+            else {
+                remoteResponse.innerText = data.mensaje;
+               
+            }
+            
+        })
+        .catch((error) => remoteResponse.innerText = 'An error occurred, might be CORS!!');
+}
+
+
+
+
